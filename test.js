9996ccd5baf055ec1ea6730b201d657428655726
Owais <warsimuhammadowais@gmail.com>
2023-11-12 10:41:52 +0530

    updated

--- test.js
+++ test.js
@@ -30,16 +30,18 @@ app.set('view engine', 'ejs');
 app.set('views', __dirname);
 
 app.get('/', (req, res) => {
-
-    res.sendFile(__dirname + "/login-page.html");
+    console.log("access")
+    res.sendFile(__dirname + "/index.html");
 })
 
 
-app.get('/login-page.html', (req, res) => {
-    res.redirect('/');
-});
+
 
 app.post("/login-page.html", (req, res) => {
+
+
+
+
     const regNo = req.body.registerNo;
     const name = req.body.username;
     const dept = req.body.dept;
@@ -65,6 +67,11 @@ app.post("/login-page.html", (req, res) => {
 
 })
 
+app.get("/login-page.html", (req, res) => {
+    res.sendFile(__dirname + '/login-page.html');
+})
+
+
 
 app.post("/voting-page.ejs", (req, res) => {
     const final_name = req.body.final_name;
@@ -75,7 +82,7 @@ app.post("/voting-page.ejs", (req, res) => {
     const sec = req.body.voter_sec;
 
     const crUpdate = 'UPDATE cr_test_table SET votingPercent = votingPercent+1 WHERE RegNo=(?)';
-    con.query(crUpdate,[final_reg],(err,succ)=>{
+    con.query(crUpdate, [final_reg], (err, succ) => {
         if (err) res.sendFile(__dirname + "/invalid-page.html");
         else console.log("crUpdated")
     })
@@ -114,19 +121,26 @@ app.get("/confirmation-page.html", (req, res) => {
 })
 
 
-app.post("/admin-login.html",(req,res)=>{
+app.post("/admin-login.html", (req, res) => {
     const sec = req.body.sec_input;
 
     const searchQuery = 'SELECT Names,votingPercent FROM cr_test_table WHERE Section = (?)';
 
-    con.query(searchQuery,[sec],(err,succ) => {
-        if(err) res.sendFile(__dirname + "/invalid-page.html");
+    con.query(searchQuery, [sec], (err, succ) => {
+        if (err) res.sendFile(__dirname + "/invalid-page.html");
         else {
-            res.render("result-page.ejs",{succ});
+            res.render("result-page.ejs", { succ });
         }
     })
 })
 
+app.get("/admin-login.html",(req,res)=>{
+    res.sendFile(__dirname + "/admin-login.html");
+})
+
+app.get("/faq.html",(req,res)=>{
+    res.sendFile(__dirname + "/faq.html");
+})
 
 app.get("/result-page.ejs", (req, res) => {
     res.render("result-page.ejs", { succ });
@@ -135,6 +149,6 @@ app.get("/result-page.ejs", (req, res) => {
 
 
 
-app.listen(8002, () => {
+app.listen(8001, () => {
     console.log("started");
 })
